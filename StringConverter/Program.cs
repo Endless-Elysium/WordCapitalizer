533d616a1f8efb46a1d680cb7caf43644b328583
Elysium Elysium <qrynryn@gmail.com>
2023-05-11 22:24:13 -0500

    Add project files.

---
+++ StringConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StringConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string g = Convert(Console.ReadLine());
+            Console.WriteLine(g);
+        }
+
+        static string Convert(string s) 
+        { 
+            string[] Out = s.Split(" ");
+            string c;
+            for (int i = 0; i < Out.Length; i++)
+            {
+                c = Out[i];
+                c = c.Replace(c[0], Char.Parse(c[0].ToString().ToUpper()));
+                Out[i] = c;
+            }
+            string result = "";
+            for (int i = 0;i < Out.Length;i++)
+            {
+                result += Out[i] + " ";
+            }
+            return result;
+        }
+    }
+}
