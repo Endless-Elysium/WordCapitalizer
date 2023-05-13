083a864f6709666dad42b9f4baf6628d02b6310d
Elysium Elysium <qrynryn@gmail.com>
2023-05-12 19:44:12 -0500

    Fixed bugs

--- StringConverter/Program.cs
+++ StringConverter/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter text");
             string g = Convert(Console.ReadLine());
             Console.WriteLine(g);
         }
@@ -11,11 +12,24 @@
         static string Convert(string s) 
         { 
             string[] Out = s.Split(" ");
+            if (Out == null) return "No vaild text found";
             string c;
             for (int i = 0; i < Out.Length; i++)
             {
+                if (Out[i] == "" || Out[i] == " ")
+                {
+                    i++;
+                }
+                try
+                {
+                    if (Out[i] == "" || Out[i] == " ") return "No vaild text found";
+                } catch { return "No vaild text found"; }
+                
                 c = Out[i];
-                c = c.Replace(c[0], Char.Parse(c[0].ToString().ToUpper()));
+                string b = c[0].ToString().ToUpper();
+                c = c.Remove(0, 1);
+                c = c.Insert(0, b);
+                //c = c.Replace(c[0], Char.Parse(c[0].ToString().ToUpper()));
                 Out[i] = c;
             }
             string result = "";
